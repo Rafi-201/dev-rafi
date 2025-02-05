b1522bca7c6178e53e913ea0e9d017fd79e05286
rafi <tanvir.hasan.rafi.00@gmail.com>
2025-02-05 17:14:10 +0600

    update build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  build :{
-    "outDir": "docs"
+  base: './', // Ensure all paths are relative (important for deployment)
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'docs', // Output all files to the docs folder
+    rollupOptions: {
+      output: {
+        // This ensures that files will be placed directly in the docs folder, not in subfolders
+        assetFileNames: '[name][extname]', // Directly place assets in docs folder
+        chunkFileNames: '[name].js', // Directly place chunked JS in docs folder
+        entryFileNames: '[name].js', // Directly place entry JS in docs folder
+      }
+    },
   }
 })
